c03a3952f6217eb9f330fc8f3cf4bf0773702396
Riyaz Usman <riyazusman@gmail.com>
2020-10-26 03:51:38 +0530

    Adding SAML Response

--- src/main/java/com/salesforce/saml/SAMLServlet.java
+++ src/main/java/com/salesforce/saml/SAMLServlet.java
@@ -242,6 +242,7 @@ public class SAMLServlet extends HttpServlet{
 
         //Get the SAMLResponse and RelayState
         String encodedResponse = request.getParameter("SAMLResponse");
+		String decodedResponse = new String(Base64.decodeBase64(encodedResponse.getBytes("UTF-8")),"UTF-8");
         String relayState = request.getParameter("RelayState");
         if ((relayState == null) || ( relayState.equals(""))) relayState = "/";
 
@@ -260,7 +261,7 @@ public class SAMLServlet extends HttpServlet{
                 identityJSON.put(key, (ArrayList<String>) attributes.getValues(key));
             }
             Cookie identityCookie = new Cookie("IDENTITY", Base64.encodeBase64URLSafeString(identityJSON.toString().getBytes("UTF-8")));
-			Cookie samlResponseCookie = new Cookie("SAMLRESPONSE", encodedResponse);
+			Cookie samlResponseCookie = new Cookie("SAMLRESPONSE", decodedResponse);
 			response.addCookie(samlResponseCookie);
             response.addCookie(identityCookie);
         } catch (Exception e) {
